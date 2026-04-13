ff23f528eb7a415c21b01b0505aa07e79f49852d
Sri Ganesh <sri.ganesh@privasapien.com>
2026-04-13 11:15:01 +0530

    feat: Added logo for the signup page

--- features/auth/components/SignupPage.tsx
+++ features/auth/components/SignupPage.tsx
@@ -3,35 +3,43 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "../hooks";
+import { LogoWithName } from "@/components/shared/Logo";
 
 const SignupPage = () => {
-  const { formData, handleChange, handleSubmit ,error} = useAuth();
+  const { formData, handleChange, handleSubmit, error } = useAuth();
 
   return (
-    <div className="space-y-3 max-w-md">
-      <Input
-        name="name"
-        placeholder="Name"
-        value={formData.name}
-        onChange={handleChange}
-      />
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="space-y-3 max-w-sm w-full flex flex-col justify-center items-center">
+        <LogoWithName className="h-8 w-8" />
 
-      <Input
-        name="email"
-        placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
-      />
+        <Input
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
 
-      <Input
-        name="password"
-        type="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-      />
+        <Input
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          className="p-4"
+        />
+
         {error && <div>{error}</div>}
-      <Button onClick={handleSubmit}>Sign Up</Button>
+
+        <Button onClick={handleSubmit} className="w-full">Sign Up</Button>
+      </div>
     </div>
   );
 };
